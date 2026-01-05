013b494efce0d4e1996485c2d9222738726732b9
leeefigueiredo <leeeandrofigueiredo@gmail.com>
2026-01-05 07:05:15 -0300

    Exercícios Vetores 3 e 4

---
+++ Sec10_97_exerc_vetores_04/src/Aplication/Program.java
@@ -0,0 +1,55 @@
+package Aplication;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.printf("quantos números irá digitar: ");
+		int n = sc.nextInt();
+		
+		Product[] vect = new Product[n];
+		
+		for (int i = 0; i < vect.length; i++) {
+			System.out.printf("digite um número: ");
+			
+			int number = sc.nextInt();
+			vect[i] = new Product(number);
+		}
+		
+		System.out.println(" ");
+		System.out.printf("números pares:");
+		System.out.println(" ");
+		
+		for (int i = 0; i < vect.length; i++) {
+			if(vect[i].getNumber() % 2 == 0) {
+				System.out.printf(vect[i].getNumber() + " ");	
+			}			
+			
+		}
+		
+		int soma = 0;
+		
+		System.out.println(" ");
+		
+		for (int i = 0; i < vect.length; i++) {
+			if(vect[i].getNumber() % 2 == 0) {
+				soma ++; 
+				
+			}			
+			
+		}
+		
+		System.out.println(" ");
+		System.out.printf("quantidade de pares: " + soma);
+		
+		sc.close();
+	}
+}
---
+++ Sec10_97_exerc_vetores_04/src/entities/Product.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class Product {
+	
+	private int number;
+
+	public Product(int number) {
+		super();
+		this.number = number;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	
+	
+}
